1f031cd255849ea01b6696754e53b86599f54695
gwbres <guillaume.bressaix@gorgy-timing.fr>
2021-12-23 15:40:28 +0100

    working on utilities/tools

--- src/utilities.rs
+++ src/utilities.rs
@@ -17,20 +17,32 @@ pub fn diff (data: Vec<f64>, normalization: Option<f64>) -> Vec<f64> {
     ret    
 }
 
+/// normalizes data[] by 1/norm.   
+pub fn normalize (data: Vec<f64>, norm: f64) -> Vec<f64> {
+    let mut data = data.clone();
+    for i in 0..data.len() {
+        data[i] /= norm
+    }
+    data
+}
+
+/// Macro to convert frequency data (Hz) to fractionnal frequency (n.a) 
+pub fn to_fractionnal_frequency (frequency: Vec<f64>, f_0: f64) -> Vec<f64> {  normalize(frequency, f_0) }
+
 /// Integrates fractionnal data (n.a).
 /// data: raw fractionnal data (n.a)   
 /// sample_rate: sampling rate (Hz) during fract acquisition   
 /// returns: integrated data ((s) if input is fract. frequency)  
 pub fn fractionnal_integral (data: Vec<f64>, sample_rate: f64) -> Vec<f64> {
     let dt = 1.0_f64 / sample_rate;
-    let mean = statistical::mean(&data);
+    //let mean = statistical::mean(&data);
     // Substract mean value before cumsum
     // in order to avoir precision issues when we have
     // small frequency fluctuations on a large average frequency
-    let mut data = data.clone();
-    for i in 0..data.len() {
-        data[i] -= mean
-    }
+    //let mut data = data.clone();
+    //for i in 0..data.len() {
+    //    data[i] -= mean
+    //}
     cumsum(data, Some(dt))
 }
 
@@ -61,27 +73,34 @@ mod tests {
     fn test_cumsum() {
         let input: Vec<f64> = vec![1.0_f64,1.0_f64,1.0_f64,1.0_f64];
         let output = cumsum(input, None);
-        println!("{:#?}", output)
+        assert_eq!(output, vec![1.0_f64,2.0_f64,3.0_f64,4.0_f64]);
     }
     
     #[test]
     fn test_fractionnal_integral() {
         let input: Vec<f64> = vec![1.0_f64,1.0_f64,1.0_f64,1.0_f64];
         let output = fractionnal_integral(input, 1.0_f64);
-        println!("{:#?}", output)
+        assert_eq!(output, vec![1.0_f64,2.0_f64,3.0_f64,4.0_f64]);
     }
     
     #[test]
     fn test_diff() {
         let input: Vec<f64> = vec![1.0_f64,2.0_f64,3.0_f64,4.0_f64];
         let output = diff(input, None);
-        println!("{:#?}", output)
+        assert_eq!(output, vec![1.0_f64,1.0_f64,1.0_f64]);
     }
     
     #[test]
     fn test_derivative() {
-        let input: Vec<f64> = vec![1.0_f64,1.0_f64,1.0_f64,1.0_f64];
+        let input: Vec<f64> = vec![1.0_f64,2.0_f64,3.0_f64,4.0_f64];
         let output = derivative(input, 1.0_f64);
-        println!("{:#?}", output)
+        assert_eq!(output, vec![1.0_f64,1.0_f64,1.0_f64]);
+    }
+    
+    #[test]
+    fn test_normalization() {
+        let input: Vec<f64> = vec![1.0_f64,2.0_f64,3.0_f64,4.0_f64];
+        let output = normalize(input.clone(), 0.5_f64);
+        assert_eq!(output, vec![2.0_f64,4.0_f64,6.0_f64,8.0_f64]);
     }
 }
